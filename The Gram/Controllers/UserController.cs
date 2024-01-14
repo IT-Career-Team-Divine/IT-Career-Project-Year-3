e366c7c0d87614f008f5165982d2b4baa02aa56c
Stefan367 <stefan.ivanov673@gmail.com>
2024-01-14 22:47:21 +0200

    Implemented Login and Logout functionality

--- The Gram/Controllers/UserController.cs
+++ The Gram/Controllers/UserController.cs
@@ -63,5 +63,52 @@ namespace The_Gram.Controllers
             }
             return View(model);
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.Username);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
---
+++ The Gram/Models/User/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace The_Gram.Models.User
+{
+    public class LoginViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Username { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
